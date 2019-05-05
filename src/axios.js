8359a797bd02966407ffe5f95e15ad1bfef6271f
handsomewjl <1946510706@qq.com>
2019-05-05 22:15:46 +0800

    ddd

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'http://127.0.0.1';
+axios.defaults.baseURL = 'http://47.100.235.95';
 axios.interceptors.request.use(function (config) {
     // 在发送请求之前做些什么
     config.params = {
